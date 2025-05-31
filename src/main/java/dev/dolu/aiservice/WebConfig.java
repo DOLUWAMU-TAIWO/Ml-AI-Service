89e653da9b20312af0bdd56827a566dcc40271b5
DOLUWAMU-TAIWO <91460092+DOLUWAMU-TAIWO@users.noreply.github.com>
2025-05-31 17:47:08 +0200

    cors config

---
+++ src/main/java/dev/dolu/aiservice/WebConfig.java
@@ -0,0 +1,22 @@
+package dev.dolu.aiservice;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins(
+                "http://localhost:5173",
+                "https://zennest.live",
+                "https://zennest.dev",
+                "https://zennest.africa"
+            )
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*");
+    }
+}
+
